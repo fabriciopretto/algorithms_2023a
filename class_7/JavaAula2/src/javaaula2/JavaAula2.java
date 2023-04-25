e24fafa8a0c3769b2ebae288e563c785dc3ff6cb
Fabrício Pretto <pretto.f@gmail.com>
2023-04-24 22:29:41 -0300

    Class 7

---
+++ class_7/JavaAula2/src/javaaula2/JavaAula2.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaaula2;
+
+/**
+ *
+ * @author pretto
+ */
+public class JavaAula2 {
+    // VAR
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // tipos de dados
+        int idade = 10;             // numeros inteiros 
+        double salario = 1250.78;   // numeros com casas decimais
+        String nome = "Juca Bala";  // sequencias de caracteres
+        char genero = '.';          // apenas 1 caractere
+        boolean tem_juca = false;   // verdadeiro (true) ou falso (false)
+
+        // utilizar notacao CamelCase
+        boolean temJuca;
+        String nomePessoa;
+        String endereco = "Rua Avelino Tallini, 171";
+        String cep = "95940-000";
+
+        // multiplas vars
+        int idade1, idade2, idade3;
+        String nome1, nome2, nome3;
+
+        System.out.println("Alguma coisa");
+        //Escreval ("");
+
+        if (idade == 0) { // simbolo == eh comparacao
+            System.out.println("Idade vale zero");
+        } else {
+            System.out.println("Idade é diferente de zero: " + idade);
+        }
+
+        System.out.println("Endereço: " + endereco);
+    }
+
+}
